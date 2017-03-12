f23e65b1311d440f639844bf624796ddaa45fe44
Minh Tang Q <minh.tang@asiantech.vn>
2017-03-12 13:33:19 +0700

    delete file

--- user/config/config.rb
+++
@@ -1,6 +0,0 @@
-db_host     = "localhost"
-db_user     = "root"
-db_password = "Quocminh27!@#"
-db_database = "user"
-DB = Database.new(db_host, db_user, db_password, db_database)
-
--- user/index.rb
+++
@@ -1,8 +0,0 @@
-# Setup
-require 'csv'
-require_relative "core/DB"
-require_relative "config/config"
-require_relative "module/read_file"
-require_relative "controller/user_controller"
-
-UserController.new.run
